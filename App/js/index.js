0753497152eb5b999e45f9578eb956ac8ee61573
arielcicom <57439264+arielcicom@users.noreply.github.com>
2019-11-06 10:23:33 +0000

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,16 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.PageBar")
+                .setHost(host,"xui_ui_pagebar1")
+                .setLeft("0em")
+                .setTop("2.5em")
+                .setWidth("66.16666666666667em")
+                .setCaption("PageBar")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
